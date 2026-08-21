e434b82dee261fcef2b1a8621646e857278ce221
aditya-core <theadityaprakash8959@gmail.com>
2026-08-21 23:23:05 +0530

    Major learnings in Syntax and variable assignment

--- syntax.c
+++ syntax.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>                 // Include the standard input-output header file
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main() {                       // printf() comes from this stdio.h
+    printf("Hello, World!\n");     // int means the function returns an integer value
+    return 0;                      // if all is well, return the value 0 to the operating system and ends main() function
 }
+
+
+
+
+
+
+
+
+
+
---
+++ variables.c
@@ -0,0 +1,22 @@
+int age = 17;
+float height = 5.8;
+double pi = 3.141592;
+char grade = 'A';
+
+
+int a= 12;
+float b=2.14;
+double c= 4.76526656868765456 ;
+char d= 'A';
+
+int main() {
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Value of Pi: %.6f\n", pi);
+    printf("Grade: %c\n", grade);
+    return 0;
+}
+
+
+
+
