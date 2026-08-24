9e97b66147647d743866b6fbb2423715247eef8a
aditya-core <theadityaprakash8959@gmail.com>
2026-08-24 12:05:34 +0530

    New topics updated and minor changes

---
+++ multi-input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int age ;
+    float height;
+    scanf("%d %f", &age, &height);
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    return 0;
+}
